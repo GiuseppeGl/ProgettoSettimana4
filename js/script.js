9de0da062cc984c5e0cca408af47a0f0c6099fdd
Giuseppe Longo <longorsm68@gmail.com>
2023-11-27 18:48:45 +0100

    c

---
+++ js/script.js
@@ -0,0 +1,19 @@
+ window.addEventListener('scroll', function() {
+    var section = document.querySelector('section');
+    var sectionBottom = section.offsetTop + section.offsetHeight + 5;
+
+    var header = document.querySelector('header');
+    var started = document.getElementById('started');
+
+    if (window.scrollY >= sectionBottom) {
+        header.style.backgroundColor = 'white';
+        started.style.backgroundColor = 'green';
+    } else {
+        header.style.backgroundColor = 'yellow';
+        started.style.backgroundColor = 'green';
+    }
+}); 
+
+ 
+
+
